8f2eb826a2f69d52c10b8b78cd690957ca97aa86
Prithiraj Sengupta <bigcat@localhost.localdomain>
2015-01-18 21:34:43 -0500

    domain list updated

--- src/com/ben/dl/listd/DomainList.java
+++ src/com/ben/dl/listd/DomainList.java
@@ -126,7 +126,7 @@ public class DomainList extends Configured implements Tool{
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(LongWritable.class);
         
-        job.setInputFormat(WarcFileInputFormat.class);
+        job.setInputFormat(WARCFileInputFormat.class);
         job.setOutputFormat(TextOutputFormat.class);
         
         job.setMapperClass(DomainListMapper.class);
